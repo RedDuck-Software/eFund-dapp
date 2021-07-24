508c2c73b9982da420cb10b1f5bc21ecd546750c
Julia Ihnatova <yuliia.ihnatova@redduck.io>
2021-07-24 10:37:02 +0300

    Coins price tab

--- src/components/CoinsPriceTab.vue
+++ src/components/CoinsPriceTab.vue
@@ -1,135 +1,132 @@
 <template>
-  <div>
-    <div class="bordered bg-lightest p-4">
-      <div class="d-flex flex-wrap">
-        <button class="btn btn-dark">1 day</button>
-        <button class="btn btn-light">1 week</button>
-        <button class="btn btn-light">1 month</button>
-        <button class="btn btn-light">3 month</button>
-        <button class="btn btn-light">6 month</button>
-        <button class="btn btn-light">1 year</button>
-        <button class="btn btn-light">2 years</button>
-      </div>
+  <div class="coins-price-wrap bg-lightest p-4 rounded">
+    <div class="d-flex flex-wrap">
+      <div class="badge bg-black text-white">1 day</div>
+      <div class="badge bg-light text-black">1 week</div>
+      <div class="badge bg-light text-black">1 month</div>
+      <div class="badge bg-light text-black">3 month</div>
+      <div class="badge bg-light text-black">6 month</div>
+      <div class="badge bg-light text-black">1 year</div>
+      <div class="badge bg-light text-black">2 year</div>
     </div>
+
     <ul class="list-group">
-      <li class="bg-light-gray d-flex mt-3">
+      <li class="coin bg-light-gray row no-gutters mt-3 align-items-center">
         <div class="col-md-4 px-0 d-flex">
-          <div class="token-icon d-flex justify-content-center align-items-center">
+          <div class="token-icon d-flex justify-content-center align-items-center mr-3">
             <img src="../assets/images/bnb_icon_transparent.png" />
           </div>
           <div>
-            <h3 class="text-black">Binance</h3>
-            <span class="text-black">$308.29 USD </span>
+            <div class=" coin-title font-weight-bold text-black">Binance</div>
+            <h2 class="text-black">$308.29 USD</h2>
           </div>
         </div>
-
-        <div class="col-md-2">
-          <div class="label text-gray">USD</div>
-          <div class="text-primary">+1.42%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class="text-gray">USD</h3>
+          <h3 class=" big text-primary">+1.42%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">BNB</div>
-          <div class="text-primary">+0.48%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">BNB</h3>
+          <h3 class="big text-primary">+0.48%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">Market cap</div>
-          <div class="text-black">$51.79B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">Market cap</h3>
+          <h3 class="middle text-black">$51.79B</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray text-center">24h volume</div>
-          <div class="text-black text-center">$1.198B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray text-center">24h volume</h3>
+          <h3 class="middle text-black text-center">$1.198B</h3>
         </div>
-        <div></div>
       </li>
-      <li class=" bg-light-gray d-flex mt-3">
+      <li class="coin bg-light-gray row no-gutters mt-3 align-items-center">
         <div class="col-md-4 px-0 d-flex">
-          <div class="token-icon d-flex justify-content-center align-items-center">
+          <div class="token-icon d-flex justify-content-center align-items-center mr-3">
             <img src="../assets/images/Dai_icon.png" />
           </div>
           <div>
-            <h3 class="text-black">Dai</h3>
-            <span class="text-black">$308.29 USD </span>
+            <div class=" coin-title font-weight-bold text-black">Dai</div>
+            <h2 class="text-black">$308.29 USD</h2>
           </div>
         </div>
 
-        <div class="col-md-2">
-          <div class="label text-gray">USD</div>
-          <div class="text-primary">+1.42%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class="text-gray">USD</h3>
+          <h3 class="big text-primary">+1.42%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">BNB</div>
-          <div class="text-primary">+0.48%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">BNB</h3>
+          <h3 class="big text-primary">+0.48%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">Market cap</div>
-          <div class="text-black">$51.79B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">Market cap</h3>
+          <h3 class="big text-black">$51.79B</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray text-center">24h volume</div>
-          <div class="text-black text-center">$1.198B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">24h volume</h3>
+          <h3 class="big text-black">$1.198B</h3>
         </div>
-        <div></div>
       </li>
-      <li class=" bg-light-gray d-flex mt-3">
+
+      <li class="coin bg-light-gray row no-gutters mt-3 align-items-center">
         <div class="col-md-4 px-0 d-flex">
-          <div class="token-icon d-flex justify-content-center align-items-center">
+          <div class="token-icon d-flex justify-content-center align-items-center mr-3">
             <img src="../assets/images/busdt_icon_transparent.png" />
           </div>
           <div>
-            <h3 class="text-black">BUSDT</h3>
-            <span class="text-black">$308.29 USD </span>
+            <div class=" coin-title font-weight-bold text-black">BUSDT</div>
+            <h2 class="text-black">$308.29 USD</h2>
           </div>
         </div>
 
-        <div class="col-md-2">
-          <div class="label text-gray">USD</div>
-          <div class="text-primary">+1.42%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class="text-gray">USD</h3>
+          <h3 class="big text-primary">+1.42%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">BNB</div>
-          <div class="text-primary">+0.48%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">BNB</h3>
+          <h3 class="big text-primary">+0.48%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">Market cap</div>
-          <div class="text-black">$51.79B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">Market cap</h3>
+          <h3 class="big text-black">$51.79B</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray text-center">24h volume</div>
-          <div class="text-black text-center">$1.198B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">24h volume</h3>
+          <h3 class="big text-black">$1.198B</h3>
         </div>
-        <div></div>
       </li>
-      <li class=" bg-light-gray d-flex mt-3">
+
+      <li class="coin bg-light-gray row no-gutters mt-3 align-items-center">
         <div class="col-md-4 px-0 d-flex">
-          <div class="token-icon d-flex justify-content-center align-items-center">
+          <div class="token-icon d-flex justify-content-center align-items-center mr-3">
             <img src="../assets/images/usdt_icon_transparent.png" />
           </div>
           <div>
-            <h3 class="text-black">Tether</h3>
-            <span class="text-black">$308.29 USD </span>
+            <div class=" coin-title font-weight-bold text-black">Tether</div>
+            <h2 class="text-black">$308.29 USD</h2>
           </div>
         </div>
 
-        <div class="col-md-2">
-          <div class="label text-gray">USD</div>
-          <div class="text-primary">+1.42%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class="text-gray">USD</h3>
+          <h3 class="big text-primary">+1.42%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">BNB</div>
-          <div class="text-primary">+0.48%</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">BNB</h3>
+          <h3 class="big text-primary">+0.48%</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray">Market cap</div>
-          <div class="text-black">$51.79B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">Market cap</h3>
+          <h3 class="big text-black">$51.79B</h3>
         </div>
-        <div class="col-md-2">
-          <div class="label text-gray text-center">24h volume</div>
-          <div class="text-black text-center">$1.198B</div>
+        <div class="col-md-2 text-center px-2 font-weight-bold">
+          <h3 class=" text-gray">24h volume</h3>
+          <h3 class="big text-black">$1.198B</h3>
         </div>
-        <div></div>
       </li>
-    </ul></div
-></template>
+    </ul>
+  </div></template
+>
 
 <script>
 export default {
@@ -140,4 +137,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.coins-price-wrap {
+  .badge {
+    font-size: 18px;
+  }
+
+  .badge:not(:first-of-type) {
+    margin-left: 8px;
+  }
+}
+
+.coin {
+  padding: 10px 22px 8px 10px;
+  border-radius: 10px;
+
+  .coin-title {
+    font-size: 20px;
+    line-height: 24px;
+  }
+}
+</style>
